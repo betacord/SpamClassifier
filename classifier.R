abc67c8f294b443cd5d31bcd294dbfc78ef3d0cf
Tomasz Krzywicki <t-krzywicki@wp.pl>
2017-04-30 00:04:02 +0200

    added a filtered message vector

--- classifier.R
+++ classifier.R
@@ -1,5 +1,14 @@
 library(tm)
 library(ggplot2)
+
+get.msg <- function(path) {
+  con <- file(path, open = "rt", encoding = "latin1")
+  text <- readLines(con)
+  msg <- text[seq(which(text == "")[1] + 1, length(text), 1)] #from first empty line to end
+  close(con)
+  return(paste(msg, collapse = "\n"))
+}
+
 spam.path <- "data/spam/" #training
 spam2.path <- "data/spam_2/" #test
 easyham.path <- "data/easy_ham/"
@@ -7,10 +16,7 @@ easyham2.path <- "data/easy_ham_2/"
 hardham.path <- "data/hard_ham/"
 hardham2.path <- "data/hard_ham_2/"
 
-get.msg <- function(path) {
-  con <- file(path, open = "rt", encoding = "latin1")
-  text <- readLines(con)
-  msg <- text[seq(which(text=="")[1] + 1, length(text), 1)]
-  close(con)
-  return(paste(msg, collapse = "\n"))
-}
+spam.docs <- dir(spam.path) #filenames from directory
+spam.docs <- spam.docs[which(spam.docs != "cmds")] #without cmds files
+all.spam <- sapply(spam.docs, function(p) get.msg(paste(spam.path, p, sep = ""))) #returns vector of messages
+
