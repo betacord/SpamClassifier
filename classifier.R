95ad751df630aee390c25a8f84c56e3c791186cc
Tomasz Krzywicki <t-krzywicki@wp.pl>
2017-04-30 10:59:38 +0200

    added a new function to get term document matrix

--- classifier.R
+++ classifier.R
@@ -2,11 +2,19 @@ library(tm)
 library(ggplot2)
 
 get.msg <- function(path) {
-  con <- file(path, open = "rt", encoding = "latin1")
+  con <- file(path, open = "rt")
   text <- readLines(con)
   msg <- text[seq(which(text == "")[1] + 1, length(text), 1)] #from first empty line to end
   close(con)
   return(paste(msg, collapse = "\n"))
+}
+
+get.tdm <- function(doc.vec) {
+  doc.corpus <- Corpus(VectorSource(doc.vec))
+  control <- list(stopwords = TRUE, removePunctuation = TRUE, removeNumbers = TRUE,
+                  minDocFreq = 2) # word min 2 chars, without numbers
+  doc.tdm <- TermDocumentMatrix(doc.corpus, control) #i - word, j - document
+  return(doc.tdm)
 }
 
 spam.path <- "data/spam/" #training
@@ -19,4 +27,5 @@ hardham2.path <- "data/hard_ham_2/"
 spam.docs <- dir(spam.path) #filenames from directory
 spam.docs <- spam.docs[which(spam.docs != "cmds")] #without cmds files
 all.spam <- sapply(spam.docs, function(p) get.msg(paste(spam.path, p, sep = ""))) #returns vector of messages
+spam.tdm <- get.tdm(all.spam)
 
