6ee97f5c30df2b7f57ce561c218ec24f12636edf
Tomasz Krzywicki <t-krzywicki@wp.pl>
2017-04-30 14:57:08 +0200

    The teaching class for spam classification has been added

--- classifier.R
+++ classifier.R
@@ -12,7 +12,7 @@ get.msg <- function(path) {
 get.tdm <- function(doc.vec) {
   doc.corpus <- Corpus(VectorSource(doc.vec))
   control <- list(stopwords = TRUE, removePunctuation = TRUE, removeNumbers = TRUE,
-                  minDocFreq = 2) # word min 2 chars, without numbers
+                  minDocFreq = 2) # word min 2 times, without numbers
   doc.tdm <- TermDocumentMatrix(doc.corpus, control) #i - word, j - document
   return(doc.tdm)
 }
@@ -27,5 +27,19 @@ hardham2.path <- "data/hard_ham_2/"
 spam.docs <- dir(spam.path) #filenames from directory
 spam.docs <- spam.docs[which(spam.docs != "cmds")] #without cmds files
 all.spam <- sapply(spam.docs, function(p) get.msg(paste(spam.path, p, sep = ""))) #returns vector of messages
+
 spam.tdm <- get.tdm(all.spam)
+spam.matrix <- as.matrix(spam.tdm)
+spam.counts <- rowSums(spam.matrix) #words count in all messages
+spam.df <- data.frame(cbind(names(spam.counts), as.numeric(spam.counts)), 
+                      stringsAsFactors = FALSE)
+names(spam.df) <- c("term", "frequrency")
+spam.df$frequrency <- as.numeric(spam.df$frequrency)
+spam.occurence <- sapply(1:nrow(spam.matrix), function(i) {
+  length(which(spam.matrix[i,] > 0)) / ncol(spam.matrix)
+}) #documents percent with word / all documents
+spam.density <- spam.df$frequrency / sum(spam.df$frequrency)
+spam.df <- transform(spam.df, density = spam.density, occurence = spam.occurence)
+
+head(spam.df[with(spam.df, order(-occurence)),])
 
